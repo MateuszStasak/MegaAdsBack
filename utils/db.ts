91844e9af89c717e6942345ebef1b34d34353c11
MateuszStasak <mstasaq@gmail.com>
2022-05-20 14:37:33 +0200

    Added config example and moved DB connection data into config.

--- utils/db.ts
+++ utils/db.ts
@@ -1,10 +1,10 @@
 import { createPool } from "mysql2/promise";
-
+import {config} from "../config/config"
 
 export const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'mega_ads',
+    host: config.dbHost,
+    user: config.dbUser,
+    database: config.dbDatabase,
     namedPlaceholders: true,
     decimalNumbers: true,
 })

67b20fc824f4592a6538828988f347e5cd09d0dc
MateuszStasak <mstasaq@gmail.com>
2022-05-20 15:32:54 +0200

    Added prefix to all URLs.

--- index.ts
+++ index.ts
@@ -17,9 +17,13 @@ app.use(json());
 app.use(rateLimit({
     windowMs: 5 * 60 * 1000, // 15 minutes
     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-}))
+}));
 
-app.use('/ad', adRouter);
+const router = Router();
+
+router.use('/ad', adRouter);
+
+app.use('/api', router);
 
 app.use(handleError);
 
